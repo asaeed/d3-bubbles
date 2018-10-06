556e42d1ba0ad04f33a5b7fe22acc3036f36c2e7
Ahmad Saeed <asaeed@gmail.com>
2018-10-06 00:05:57 -0400

    positioning with simulation

--- app.js
+++ app.js
@@ -1,81 +1,76 @@
 
-var svg = d3.select("svg"),
+var svg = d3.select('svg'),
     width = 400,
     height = 400,
     color = d3.scaleOrdinal(d3.schemeCategory10);
 
-var a = {id: "a"},
-    b = {id: "b"},
-    c = {id: "c"},
-    nodes = [a, b, c],
-    links = [];
+var nodes = [
+    { id: 'a', r: 24, x: -40, y: 10 },
+    { id: 'b', r: 20, x: -20, y: 50 },
+    { id: 'c', r: 10, x: 0, y: 10 },
+    { id: 'd', r: 16, x: 40, y: 50 },
+    { id: 'e', r: 20, x: 80, y: 10 }
+];
 
 var simulation = d3.forceSimulation(nodes)
-    .force("charge", d3.forceManyBody().strength(-1000))
-    .force("link", d3.forceLink(links).distance(100))
-    .force("x", d3.forceX())
-    .force("y", d3.forceY())
+    .force('charge', d3.forceManyBody().strength(-100)) // lower is further apart
+    .force('x', d3.forceX().x(function(d) {
+        return d.x;
+    }))
+    .force('y', d3.forceY().y(function(d) {
+        return d.y;
+    }))
     .alphaTarget(1)
-    .on("tick", ticked);
+    .on('tick', ticked);
 
-var g = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")"),
-    link = g.append("g").attr("stroke", "#000").attr("stroke-width", 1.5).selectAll(".link"),
-    node = g.append("g").attr("stroke", "#fff").attr("stroke-width", 1.5).selectAll(".node");
+var g = svg.append('g').attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
+var node = g.append('g').attr('stroke', '#fff').attr('stroke-width', 1.5).selectAll('.node');
 
 restart();
 
-d3.timeout(function() {
-  links.push({source: a, target: b}); // Add a-b.
-  links.push({source: b, target: c}); // Add b-c.
-  links.push({source: c, target: a}); // Add c-a.
-  restart();
-}, 1000);
-
-d3.interval(function() {
-  nodes.pop(); // Remove c.
-  links.pop(); // Remove c-a.
-  links.pop(); // Remove b-c.
-  restart();
-}, 2000, d3.now());
-
-d3.interval(function() {
-  nodes.push(c); // Re-add c.
-  links.push({source: b, target: c}); // Re-add b-c.
-  links.push({source: c, target: a}); // Re-add c-a.
-  restart();
-}, 2000, d3.now() + 1000);
-
 function restart() {
 
-  // Apply the general update pattern to the nodes.
-  node = node.data(nodes, function(d) { return d.id;});
-  node.exit().remove();
-  node = node.enter().append("circle").attr("fill", function(d) { return color(d.id); }).attr("r", 8).merge(node);
+    // Apply the general update pattern to the nodes.
+    node = node.data(nodes, function (d) { return d.id; });
+    node.exit().remove();
+    node = node.enter().append('circle')
+        .attr('fill', function (d) { return color(d.id); })
+        .attr('r', function (d) { return d.r; })
+        // .attr('cx', 10)
+        // .attr('cy', -300)
+        .merge(node);
 
-  // Apply the general update pattern to the links.
-  link = link.data(links, function(d) { return d.source.id + "-" + d.target.id; });
-  link.exit().remove();
-  link = link.enter().append("line").merge(link);
-
-  // Update and restart the simulation.
-  simulation.nodes(nodes);
-  simulation.force("link").links(links);
-  simulation.alpha(1).restart();
+    // Update and restart the simulation.
+    simulation.nodes(nodes);
+    simulation.alpha(1).restart();
 }
 
 function ticked() {
-  node.attr("cx", function(d) { return d.x; })
-      .attr("cy", function(d) { return d.y; })
+    node.attr('cx', function (d) { return d.x; })
+        .attr('cy', function (d) { return d.y; })
 
-  link.attr("x1", function(d) { return d.source.x; })
-      .attr("y1", function(d) { return d.source.y; })
-      .attr("x2", function(d) { return d.target.x; })
-      .attr("y2", function(d) { return d.target.y; });
 }
 
 let input = d3.select('#controls input');
 let button = d3.select('#controls button');
 
 button.node().addEventListener('click', (e) => {
-  console.log(input.node().value);
+    console.log(input.node().value);
+
+    console.log(simulation);
+
 })
+
+d3.timeout(function() {
+  restart();
+}, 2000);
+
+// d3.interval(function() {
+//   nodes.pop(); // remove e
+//   restart();
+// }, 2000, d3.now());
+
+// d3.interval(function() {
+//   nodes.push({ id: 'e', r: 20 });
+//   restart();
+// }, 2000, d3.now() + 1000);
